b5011c4cefe17659da8666ebf62fefbd42b66974
Axuata <axuata@gmail.com>
2025-01-09 21:58:29 +0900

    a

---
+++ src/main/java/dev/axuata/recipe_plus/RecipePlus.java
@@ -0,0 +1,10 @@
+package dev.axuata.recipe_plus;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(RecipePlus.MODID)
+public class RecipePlus {
+    public static final String MODID = "recipe_plus";
+}
